f224c2caf00c165c0eb9c5e1710b73dbd71c5259
Max Schmeling <max@clipisode.com>
2020-03-16 13:56:59 -0500

    Delay native module import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { NativeModules } from "react-native";
 
-const { VideoComposer } = NativeModules;
-
 type Composition = {
   duration: number;
   videos: Array<{ path: string; startAt: number }>
 };
 
 export function compose(composition: Composition, output: string): Promise<string> {
+  const { VideoComposer } = NativeModules;
+
   return VideoComposer.compose(composition, output);
 }
